0eedff2a253b48dedb08f85011cb8e6d41743041
Eluviian <bayleysapara@gmail.com>
2024-01-22 17:26:56 -0700

    added trivial test

---
+++ smart-home/Platform/test/ci_test.java
@@ -0,0 +1,14 @@
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Assert;
+
+public class ci_test {
+
+    public void zero_test()
+    {
+    int x = 0;
+    assert x == 0;
+    }    
+    
+}
