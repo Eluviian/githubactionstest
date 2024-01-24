ba54b7e0a2768ea3315a9eb21bd204faae359678
Eluviian <bayleysapara@gmail.com>
2024-01-24 13:28:10 -0700

    fixing tests

---
+++ smart-home/Platform/src/test/java/ci_test.java
@@ -0,0 +1,19 @@
+
+// import org.junit.Test;
+// import org.junit.Before;
+// import org.junit.Assert;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ci_test {
+
+    @Test
+    public void zero_test()
+    {
+    int x = 0;
+    assertEquals(x,2);
+    }    
+    
+}
